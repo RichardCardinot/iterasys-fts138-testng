42eb1d02fd9a1087f1d92b0fa7f1b6c8ae599917
Richard Cardinot <rixa.git@gmail.com>
2023-06-15 22:55:01 -0300

    Utilizando o método GET para obter o usuário previamente cadastrado

--- src/test/java/apitest/Account.java
+++ src/test/java/apitest/Account.java
@@ -22,6 +22,8 @@ public class Account {
     Response resposta; // guardar o retorno da API
     String token; // guardar o token - autenticação do usuário
 
+    AccountEntity accountEntity;
+
     // 3.1.2 Instanciar Classes Externas
     Gson gson = new Gson();
 
@@ -31,29 +33,29 @@ public class Account {
     @Test(priority = 0)
     public void testCreateUser() {
         // Arrange
-        AccountEntity accountEntity = new AccountEntity(); // Instancia a entidade
+        accountEntity = new AccountEntity(); // Instancia a entidade
 
-        accountEntity.userName = "amanda"; // entrada e saida (resultado esperado)
+        accountEntity.userName = "Richardisii"; // entrada e saida (resultado esperado)
         accountEntity.password = "P@ss0rd1"; // entrada
 
         jsonBody = gson.toJson(accountEntity); // Converte a entidade usuario no formato Json
 
         // Act
         resposta = (Response) // Casting em Classes
-        given()
-                .contentType(vlContentType)  // tipo do conteúdo
-                .log().all()                      // registre tudo
-                .body(jsonBody)                  // corpo da mensagem que será enviada
-        .when()
-                .post(uri + "User")
+            given()
+                    .contentType(vlContentType)  // tipo do conteúdo
+                    .log().all()                      // registre tudo
+                    .body(jsonBody)                  // corpo da mensagem que será enviada
+            .when()
+                    .post(uri + "User")
 
-        // Assert
-        .then()
-                .log().all()     // registre tudo na volta
-                .statusCode(201) // valida a comunicação
-                .body("username", is(accountEntity.userName)) // valida o usuário
-        .extract()
-        ; // fim da linha do REST-assured
+            // Assert
+            .then()
+                    .log().all()     // registre tudo na volta
+                    .statusCode(201) // valida a comunicação
+                    .body("username", is(accountEntity.userName)) // valida o usuário
+            .extract()
+            ; // fim da linha do REST-assured
 
         userId = resposta.jsonPath().getString("userID");
 
@@ -69,21 +71,21 @@ public class Account {
 
         // Act
         resposta = (Response)
-        given()
-                .contentType(vlContentType)
-                .log().all()
-                .body(jsonBody)
-        .when()
-                .post(uri + "GenerateToken")
+            given()
+                    .contentType(vlContentType)
+                    .log().all()
+                    .body(jsonBody)
+            .when()
+                    .post(uri + "GenerateToken")
 
-        // Assert
-        .then()
-                .log().all()
-                .statusCode(200) // valida a comunicação
-                .body("status", is("Success"))
-                .body("result", is("User authorized successfully."))
-        .extract()
-        ;
+            // Assert
+            .then()
+                    .log().all()
+                    .statusCode(200) // valida a comunicação
+                    .body("status", is("Success"))
+                    .body("result", is("User authorized successfully."))
+            .extract()
+            ;
 
         // Extração do Token
         token = resposta.jsonPath().getString("token");
@@ -95,4 +97,77 @@ public class Account {
 
     } // fim do método de geração de token de identificação do usuário
 
+    @Test(priority = 2)
+    public void testAuthorized() {
+        // Arrange
+            // Dados de entrada
+                // --> Fornecidos pela AccountEntity atraves do método testCreateUser()
+            // Dados de saida / Resultado Esperado
+                // --> StatusCode == 200
+                // --> Response Body == true
+
+        // Act
+            given()
+                .contentType(vlContentType)
+                .log().all()
+                .body(jsonBody)
+            .when()
+                .post(uri + "Authorized")
+
+        // Assert
+            .then()
+                .log().all()
+                .statusCode(200)
+                .body(is("true"))
+            ;
+    }
+
+    @Test(priority = 3)
+    public void testResearchUserNotAuthorized() {
+        // Arrange
+            // Dados de Entrada
+                // userId que foi extraido no método testeCreateUser()
+
+            // Dados de Saída / Resultado Esperado
+                // statusCode 400, Code 1200 e Message "User not authorized!"
+        // Act
+        given()
+            .contentType(vlContentType)
+            .log().all()
+        .when()
+            .get(uri + "User/" + userId)
+
+        // Assert
+        .then()
+            .log().all()
+            .statusCode(401)
+            .body("code", is("1200"))
+            .body("message", is("User not authorized!"))
+        ;
+    }
+
+    @Test(priority = 4)
+    public void testResearchUser() {
+        // Arrange
+            // Dados de Entrada
+                // userId que foi extraido no método testeCreateUser()
+
+            // Dados de Saída / Resultado Esperado
+                // O mesmo userName do objeto accountEntity e o statusCode deve ser 200
+
+        // Act
+        given()
+            .contentType(vlContentType)
+            .log().all()
+            .header("Authorization", "Bearer " + token) // É necessário concatenar o token com a string "Bearer" para autenticação ser feita.
+        .when()
+            .get(uri + "User/" + userId)
+
+        // Assert
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("username", is(accountEntity.userName))
+        ;
+    }
 }
--- src/test/java/entities/AccountEntity.java
+++ src/test/java/entities/AccountEntity.java
@@ -3,4 +3,6 @@ package entities;
 public class AccountEntity {
     public String userName;
     public String password;
+
+
 }
